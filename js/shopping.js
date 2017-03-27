d3c4b637b66dcb27dec2516e10e3ac87ed646fb2
ayaokudera <09drbkksst@gmail.com>
2017-03-27 14:50:39 +0000

    add git-logo image

--- js/shopping.js
+++ js/shopping.js
@@ -1,4 +1,4 @@
-// ギャラリーの画像切替
+
 $(window).load(function(){
   $("#navi img").click(function(){
     var img_src = $(this).attr("src");
@@ -7,14 +7,14 @@ $(window).load(function(){
   });
 });
 
-// ハンバーガーメニュー
+
 $(document).ready(function(){
   $("#sp_menu_btn").click(function () {
     $(".toggle").slideToggle();
   });
 });
 
-// カルーセル（ヘッダー）
+
 $(document).ready(function(){
   $('.owl-carousel').owlCarousel({
     loop:true,
